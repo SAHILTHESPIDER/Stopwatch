75a3914b1de1a15a6bb2810c52984375569841f8
SAHIL GUPTA <136927576+SAHILTHESPIDER@users.noreply.github.com>
2024-01-01 12:13:28 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+//var number1=25;
+//var number2=30;
+//console.log(number1+number2);
+///console.log(number1-number2);
+///console.log(number1*number2);
+//console.log(number1/number2);
+//console.log(number1<=number2);
+ var arr=[1,2,3,4,5,6,7];
+ console.log(arr);
+ for( var i=1;i<=arr.length;i++)
+ console.log(arr[i]);
